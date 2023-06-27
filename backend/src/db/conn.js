5fde08f0238ea2b4367b9cf6bd479a901468340e
vyshnav-chikku <vyshnavk891@gmail.com>
2023-06-27 20:30:16 +0530

    jun 27

---
+++ backend/src/db/conn.js
@@ -0,0 +1,12 @@
+// Importing dotenv library to use relevant local environment variables.
+require("dotenv").config({ path: "config.env" });
+
+// Establishing connection to the database hosted on mogodb atlas
+const mongoose = require("mongoose");
+
+console.log(process.env.DB);
+
+mongoose
+  .connect(process.env.DB)
+  .then(() => console.log("Connection is sucessful"))
+  .catch((err) => console.log(err));
